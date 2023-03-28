41ac888d28a7146bfef36ec720845e4cae7e9f45
ImeneRdn <imene.ramdan@live.fr>
2023-03-28 15:32:27 +0200

    tableaupokedex

--- my-new-app/src/components/PokemonCard.jsx
+++ my-new-app/src/components/PokemonCard.jsx
@@ -1,11 +1,37 @@
 function PokemonCard(){
-    return(
-        <div>
-            <figure>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-                <figcaption>"bulbasaur"</figcaption>
-            </figure>
-        </div>
-    )
+
+    const pokemon = pokemonList[1];
+    //condition? 
+    return pokemon.imgSrc !== undefined ?
+    
+    //si c'est oui alors va chercher l'image
+    <div>
+        <figure>
+            <img src= {pokemon.imgSrc}alt={pokemon.name} />
+            <figcaption>"{pokemon.name}</figcaption>
+        </figure>
+    </div>
+    // : 
+    :
+    //condition si c'est non
+    <div>
+        <figure>
+            <p>???</p>
+            <figcaption>{pokemon.name}</figcaption>
+        </figure>
+    </div>
+        
+    
 }
 export default PokemonCard;
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
